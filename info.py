813f5bccf3851772a5d6c30bb3638f16cf9249bc
sidbanglatips1 <167536055+sidbanglatips1@users.noreply.github.com>
2024-12-13 18:40:30 +0400

    Update info.py

--- info.py
+++ info.py
@@ -105,7 +105,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "")
+URL = environ.get("FQDN", "spicy-marjie-mongodb7575-425e4847.koyeb.app/")
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
